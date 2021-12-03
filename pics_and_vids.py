e80ac4d323fd41beb9773fee879b8a096ea5f13e
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-12-03 12:56:43 -0600

    Update pics_and_vids.py

--- pics_and_vids.py
+++ pics_and_vids.py
@@ -9,14 +9,15 @@ from bs4 import BeautifulSoup
 class MyLikes:
     def __init__(self, url, driver_path) -> None:
         self.url = url
-        self.URLS = list()
         self.saved_picture_urls = list()
         self.saved_video_urls = list()
         self.video_ids = list()
+        self.card_identifier = dict()
         self.picture_count = 0
         self.url_regEx = re.compile(pattern=r'url\(\"(https://.+\.jpg)\"')
         self.video_regEx = re.compile(pattern=r'playsinline.*src=\"(.+\.mp4)\"')
         self.video_id_regEx = re.compile(pattern=r'https://images-ssl.gotinder.com/.*\d{3}x\d{3}_(.*)\_h264\.mp4')
+        self.card_identifier_regEx = re.compile(pattern=r'https://images-ssl.gotinder.com/(.+)/\d{3}x')
         self.options = webdriver.ChromeOptions()
         self.options.add_experimental_option('debuggerAddress', 'localhost:9222')
         self.driver = webdriver.Chrome(executable_path=driver_path, options=self.options)
@@ -34,7 +35,7 @@ class MyLikes:
         time.sleep(3)
 
     def main(self):
-        for i in range(30): # This range can be pretty much anything, really. This is probably overkill, but you don't want to set it to a number where there will still be profiles to download
+        for i in range(300): # This range can be pretty much anything, really. This is probably overkill, but you don't want to set it to a number where there will still be profiles to download
             time.sleep(3)
 
             # Get the current page's HTML
@@ -46,18 +47,18 @@ class MyLikes:
             cards = self.soup.find_all('div', {'aria-label': re.compile(pattern='.*'), 'class': 'Bdrs(8px) Bgz(cv) Bgp(c) StretchedBox'})
 
             # Iterate over the profile cards
-            for card in cards:                
-                the_url = re.search(pattern=self.url_regEx, string=str(card)).group(1)
-                
-                if the_url in self.URLS:
-                    continue # Since the profile picture URL is in the list, skip this card and go to the next card
+            for card in cards:  
+                card_identifier = re.search(pattern=self.card_identifier_regEx, string=str(card)).group(1)
+
+                if self.card_identifier.get(card_identifier) is not None:
+                    continue # Since the profile card ID is in the dictionary, skip this card and go to the next card
                 else:        # Since we haven't gathered the profile data, gather now
                     # Click in the background
                     pyautogui.moveTo(x=1850, y=350, duration=0.1)
                     pyautogui.click()
 
-                    # Add the URL to the URLS list
-                    self.URLS.append(the_url)
+                    # Add the card ID to the dictionary
+                    self.card_identifier.setdefault(card_identifier, 0)
 
                     # Increment the picture count
                     self.picture_count += 1
@@ -88,7 +89,7 @@ class MyLikes:
                     pic_url_counter = 0
 
                     # Iterate over the profile's pictures/videos
-                    for j in range(1, number_of_pages, 1):
+                    for _ in range(1, number_of_pages, 1):
                         time.sleep(1)
 
                         page_html = self.driver.page_source
@@ -105,11 +106,7 @@ class MyLikes:
 
                             if pot_url in self.saved_picture_urls:
                                 continue
-                            else:
-                                # We have to append each profile's last picture's URL to the "URLS" list since the last picture is shown when clicking off a profile
-                                if j == (number_of_pages - 1):
-                                    self.URLS.append(pot_url)
-
+                            else:                               
                                 pic_url_counter += 1
 
                                 self.saved_picture_urls.append(pot_url)
@@ -154,13 +151,13 @@ class MyLikes:
             if i == 0:
                 pyautogui.moveTo(x=1850, y=350, duration=0.5)
                 time.sleep(1)
-                print(f'Run number: {i}')
+                print(f'Run number: {i} | {pyautogui.position()}')
                 pyautogui.scroll(clicks=-2000)
                 time.sleep(2.5)
                 pyautogui.scroll(clicks=-280)
                 time.sleep(1)
             else:
-                print(f'Run number: {i}')
+                print(f'Run number: {i} | {pyautogui.position()}')
                 time.sleep(2.5)
                 pyautogui.scroll(clicks=-755)
                 time.sleep(2.5)
