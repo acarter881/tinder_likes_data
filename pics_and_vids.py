f4f6efeab98c199c42b14b27a76ec422c59195af
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-12-05 16:36:01 -0600

    Update pics_and_vids.py

--- pics_and_vids.py
+++ pics_and_vids.py
@@ -24,7 +24,7 @@ class MyLikes:
         self.driver = webdriver.Chrome(executable_path=driver_path, options=self.options)
         self.headers = {
                     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-                    'accept-language': 'en, en-US',
+                    'accept-language': 'en-US,en;q=0.9',
                     'referer': 'https://tinder.com/',
                     'dnt': '1'
         }
