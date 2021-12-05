413fb6955f3d8120452dba5c70217330e7370b09
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-12-05 14:15:27 -0600

    Update tinder_analysis.py

--- tinder_analysis.py
+++ tinder_analysis.py
@@ -74,10 +74,12 @@ class TinderData:
         # Punctuations and uninteresting words to process text
         punctuations = '''!()-.[]{};:'"\,<>./?@#$%^&*_~'''
     
-        uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
-        "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
-        "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
-        "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+        uninteresting_words = ["a", 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        "the", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", 
+        "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", 
+        "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", 
+        "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", 
         "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
 
         # Create a text list that ignores the "None" values
@@ -103,10 +105,8 @@ class TinderData:
       
         # Create the word cloud
         cloud = WordCloud(font_path=r'C:\Windows\Fonts\HoboStd.otf', max_words=200, max_font_size=50, min_font_size=0, random_state=5, colormap='autumn', mask=mask).generate_from_frequencies(Counter(words))
-
         cloud.to_file('my_cloud.png')
 
-        
 if __name__ == '__main__':
     c = TinderData(path_to_workbook=r'C:\Users\Alex\Downloads\tinder - Excel workbooks\Tinder_Card_Data.xlsx')
     c.load_workbook()
