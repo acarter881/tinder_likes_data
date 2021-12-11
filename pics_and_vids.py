c19104f7d1a5d04634fb600a288a64030fc9ace2
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-12-11 17:49:08 -0600

    Update pics_and_vids.py
    Updating the logic for iterating over the profile cards and downloading the pictures and videos.
    
    No longer downloading a still image of the video in addition to the video.
    
    Fixed bug where script only downloads h264 encoded videos.
    
    Adding data to get ready for scraping the datetime from the response headers.

--- pics_and_vids.py
+++ pics_and_vids.py
@@ -4,25 +4,27 @@ import pyautogui
 import requests
 import sys
 import random
+import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 class MyLikes:
-    def __init__(self, url, driver_path) -> None:
-        self.url = url
-        self.saved_picture_urls = list()
-        self.saved_video_urls = list()
-        self.video_ids = list()
-        self.card_identifier = dict()
-        self.picture_count = 0
+    def __init__(self, url, driver_path, records_path) -> None:
+        self.url = url                      # URL for selenium
+        self.complete = False               # Is the script finished?
+        self.incrementer = 0                # Variable to replace a count within a for loop for `main`
+        self.card_identifier = dict()       # Unique identifier for a profile card
+        self.picture_count = 0              # This helps to identify the profile card we're on and is also used in the filenames
+        self.records = list()               # Storing the data to be written to an Excel workbook
+        self.records_path = records_path    # Path to save the Excel workbook
+        self.now = datetime.utcnow()        # Store the start time of the script in a variable
         self.url_regEx = re.compile(pattern=r'url\(\"(https://.+\.jpg)\"')
-        self.video_regEx = re.compile(pattern=r'playsinline.*src=\"(.+\.mp4)\"')
-        self.video_id_regEx = re.compile(pattern=r'https://images-ssl.gotinder.com/.*\d{3}x\d{3}_(.*)\_h264\.mp4')
         self.card_identifier_regEx = re.compile(pattern=r'https://images-ssl.gotinder.com/(.+)/\d{3}x')
-        self.options = webdriver.ChromeOptions()
-        self.options.add_experimental_option('debuggerAddress', 'localhost:9222')
-        self.driver = webdriver.Chrome(executable_path=driver_path, options=self.options)
-        self.headers = {
+        self.options = webdriver.ChromeOptions() # Standard for using Chrome with selenium
+        self.options.add_experimental_option('debuggerAddress', 'localhost:9222') # Running Chrome on localhost
+        self.driver = webdriver.Chrome(executable_path=driver_path, options=self.options) # Standard for using Chrome with selenium
+        self.headers = { # Headers for our requests. These are very important. Without them, we can get timed out or banned.
                     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
                     'accept-language': 'en-US,en;q=0.9',
                     'referer': 'https://tinder.com/',
@@ -37,12 +39,12 @@ class MyLikes:
         self.driver.get(url=self.url)
         time.sleep(4)
                                                
-        # Click the Likes Sent button
-        self.driver.find_element_by_xpath(xpath='//*[@id="q-1452937969"]/div[1]/div[2]/a').click() # Tinder may change the div the xpath relates to. I can probably write a regular expression to account for this, but I manually updated this one.
+        # Click the Likes Sent button           
+        self.driver.find_element_by_xpath(xpath='//a[@href="/app/my-likes"]').click() # Selecting by the href of an anchor (i.e., 'a') tag
         time.sleep(3)
 
     def main(self) -> None:
-        for i in range(999): # This range can be pretty much anything, really. This is probably overkill, but you don't want to set it to a number where there will still be profiles to download
+        while not self.complete:
             time.sleep(3)
 
             # Get the current page's HTML
@@ -51,8 +53,12 @@ class MyLikes:
             # Create a soup object
             self.soup = BeautifulSoup(final_html, 'html.parser')
 
-            cards = self.soup.find_all('div', {'aria-label': re.compile(pattern='.*'), 'class': 'Bdrs(8px) Bgz(cv) Bgp(c) StretchedBox'})
+            # Find all profile cards within the current HTML
+            cards = self.soup.find_all('div', {'aria-label': re.compile(pattern=r'.*'), 'class': 'Bdrs(8px) Bgz(cv) Bgp(c) StretchedBox'})
 
+            # Find the div's id for the div that holds the profile cards. This is important because Tinder frequently changes this id 
+            div_id = self.soup.find('div', {'class': 'likesYou__scroller Sb(s) D(f) Jc(c) Fxd(c) Animtf(l) Animfm(f) Animdur(.75s) Ovy(s) Ovsb(n) Ovs(touch)'})['id']
+            
             # Iterate over the profile cards
             for card in cards:  
                 card_identifier = re.search(pattern=self.card_identifier_regEx, string=str(card)).group(1)
@@ -71,14 +77,15 @@ class MyLikes:
                     self.picture_count += 1
 
                     # Click the relevant profile card           
-                    if self.driver.find_element_by_xpath(xpath=f'//*[@id="q438038533"]/div[2]/div[{self.picture_count}]/div/div/span/div') is not None: # Tinder may change the div the xpath relates to. I can probably write a regular expression to account for this, but I manually updated this one.
+                    if self.driver.find_element_by_xpath(xpath=f'//*[@id="{div_id}"]/div[2]/div[{self.picture_count}]/div/div/span/div') is not None: # Tinder may change the div the xpath relates to. I can probably write a regular expression to account for this, but I manually updated this one.
                         try:
-                            self.driver.find_element_by_xpath(xpath=f'//*[@id="q438038533"]/div[2]/div[{self.picture_count}]/div/div/span/div').click()
+                            self.driver.find_element_by_xpath(xpath=f'//*[@id="{div_id}"]/div[2]/div[{self.picture_count}]/div/div/span/div').click()
                         except Exception as e:
-                            self.driver.find_element_by_xpath(xpath=f'//*[@id="q438038533"]/div[2]/div[{self.picture_count}]/div/div/span/div[2]/video').click()
-                    elif self.driver.find_element_by_xpath(xpath=f'//*[@id="q438038533"]/div[2]/div[{self.picture_count}]/div/div') is not None:
-                        self.driver.find_element_by_xpath(xpath=f'//*[@id="q438038533"]/div[2]/div[{self.picture_count}]/div/div').click()
+                            self.driver.find_element_by_xpath(xpath=f'//*[@id="{div_id}"]/div[2]/div[{self.picture_count}]/div/div/span/div[2]/video').click()
+                    elif self.driver.find_element_by_xpath(xpath=f'//*[@id="{div_id}"]/div[2]/div[{self.picture_count}]/div/div') is not None:
+                        self.driver.find_element_by_xpath(xpath=f'//*[@id="{div_id}"]/div[2]/div[{self.picture_count}]/div/div').click()
                     else:
+                        self.complete = True
                         sys.exit('The script is complete. There are no more profile cards to go through.')
                                      
                     time.sleep(1)
@@ -93,90 +100,76 @@ class MyLikes:
                     # Get the total number of pages in the profile card
                     try:
                         number_of_pages = int(second_soup.find('button', {'class': 'bullet D(ib) Va(m) Cnt($blank)::a D(b)::a Cur(p) bullet--active H(4px)::a W(100%)::a Py(4px) Px(2px) W(100%) Bdrs(100px)::a Bgc(#fff)::a focus-background-style'}).text.split('/')[1])
-                    except Exception as e:  
-                        number_of_pages = 2 # If the page only has one page, set this to 2 so the loop below will run one time, as the loop starts at 1
+                    except Exception:  
+                        number_of_pages = 1 # If there's only one page, there won't be a button.
 
-                    pic_url_counter = 0
-
-                    # Iterate over the profile's pictures/videos
-                    for _ in range(1, number_of_pages, 1):
+                    # Iterate over the number of pages
+                    for i in range(0, number_of_pages, 1):
                         time.sleep(1)
 
                         page_html = self.driver.page_source
 
                         page_soup = BeautifulSoup(page_html, 'html.parser')
 
-                        pot_pics = page_soup.find_all('div', {'class': 'profileCard__slider__img Z(-1)'})
+                        current_card = page_soup.find('span', {'class': 'keen-slider__slide Wc($transform) Fxg(1)', 'aria-hidden': 'false', 'style': re.compile(pattern=r'.+')})
 
-                        pot_vids = page_soup.find_all('video', {'class': 'W(100%)'}) 
+                        vid = current_card.find('video', {'class': 'W(100%)'})
 
-                        # Download available profile pictures
-                        for item in pot_pics:
-                            pot_url = re.search(pattern=self.url_regEx, string=str(item)).group(1)
+                        if vid: 
+                            vid = vid['src']
+                            download_url = vid
+                        else:
+                            pic = re.search(pattern=self.url_regEx, string=str(current_card)).group(1)
+                            download_url = pic
 
-                            if pot_url in self.saved_picture_urls:
-                                continue
-                            else:                               
-                                pic_url_counter += 1
+                        r = requests.get(url=download_url, headers=self.headers)     
 
-                                self.saved_picture_urls.append(pot_url)
+                        # content_type, res_date, res_last_mod, e_tag = r.headers['Content-Type'], r.headers['Date'], r.headers['Last-Modified'], r.headers['ETag'].strip('\"')
+                        # res_last_mod = self.to_datetime_obj(date_str=res_last_mod)
+                        # self.records.append((self.picture_count, name, card_identifier, content_type, res_date, res_last_mod, self.now, e_tag)) 
 
-                                r = requests.get(url=pot_url, headers=self.headers)
-
-                                #print(r.headers) # The 'Content-Type' is either 'image/jpeg' (picture) or 'application/octet-stream' (video). The date the user uploaded the picture/video is found in 'Last-Modified'.
-
-                                with open(file=f'./tinder_pics/{self.picture_count}_{name}_{pic_url_counter}.jpg', mode='wb') as pic:
-                                    pic.write(r.content)
-                        
-                        # Download a video, if available
-                        if pot_vids: # Only going to recognize and download the "h264" videos
-                            for item in pot_vids:
-                                try:
-                                    pot_vid_url = re.search(pattern=self.video_regEx, string=str(item)).group(1)
-
-                                    vid_id = re.search(pattern=self.video_id_regEx, string=pot_vid_url).group(1)
-
-                                    if vid_id in self.video_ids:
-                                        pass
-                                    else:
-                                        self.video_ids.append(vid_id)
-
-                                        r = requests.get(url=pot_vid_url, headers=self.headers)
-
-                                        with open(file=f'./tinder_pics/{self.picture_count}_{name}_video_{pic_url_counter}.mp4', mode='wb') as vid:
-                                            vid.write(r.content)
-                                except Exception as e:
-                                    pass
-                                        
-                        # Click to go to the next page
-                        pyautogui.moveTo(x=1250, y=400, duration=0.1)
-                        pyautogui.click()
-
-                        time.sleep(1)
-
-                    # When done going through the pictures/videos, click off of the profile card
+                        with open(file=f'./tinder_pics/{self.picture_count}_{name}_{i+1}.{download_url[-3:]}', mode='wb') as file:
+                            file.write(r.content)   
+                         
+                        if i != (number_of_pages - 1):
+                            pyautogui.moveTo(x=1250, y=400, duration=0.1)
+                            pyautogui.click()
+                            time.sleep(1)
+                        else:
+                            continue
+                                       
                     pyautogui.moveTo(x=1850, y=350, duration=0.1)
                     pyautogui.click()
 
                     time.sleep(1)
 
-            if i == 0:
+            if self.incrementer == 0:
                 pyautogui.moveTo(x=1850, y=350, duration=0.5)
                 time.sleep(1)
-                print(f'Run number: {i} | {pyautogui.position()}')
+                print(f'Run number: {self.incrementer} | {pyautogui.position()}')
                 pyautogui.scroll(clicks=-2000)
                 time.sleep(2.5)
                 pyautogui.scroll(clicks=-280)
                 time.sleep(1)
+                self.incrementer += 1
             else:
-                print(f'Run number: {i} | {pyautogui.position()}')
+                print(f'Run number: {self.incrementer} | {pyautogui.position()}')
                 time.sleep(random.randint(2, 3))
                 pyautogui.scroll(clicks=-755)
                 time.sleep(random.randint(2, 3))
-            
+                self.incrementer += 1
+
+    def to_datetime_obj(self, date_str) -> datetime.strptime:
+        return datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S %Z') 
+    
+    def pandas_to_excel(self) -> None:
+        self.df = pd.DataFrame(data=self.records, columns=['Name', 'Age', 'Distance', 'Bio', 'Passions', 'Song_Title', 'Song_Artist'])
+        self.df.to_excel(excel_writer=self.records_path, sheet_name='Tinder Card Data', index=False, freeze_panes=(1,0))
+              
 # Instantiate the class and call the necessary functions, inputting your arguments
 if __name__ == '__main__':
     c = MyLikes(url='https://tinder.com/app/recs', 
-                driver_path=r'C:\Users\Alex\Desktop\Python drivers\chromedriver.exe')
+                driver_path=r'C:\Users\Alex\Desktop\Python drivers\chromedriver.exe',
+                records_path=r'C:\Users\Alex\Desktop\Date_Test.xlsx')
     c.log_in()
     c.main()
