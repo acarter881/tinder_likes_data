a8fdf326f09e05e0c1b4c4d119505bd5b540827c
Alex Carter <60497471+acarter881@users.noreply.github.com>
2022-01-22 22:21:30 -0600

    Update hash_images.py
    Adding some more details for a separate hash scenario.

--- hash_images.py
+++ hash_images.py
@@ -1,9 +1,10 @@
 import imagehash
 import os
+import pandas as pd
 from PIL import Image
 from tqdm import tqdm
 
-def hash_images(directory: str, hashes=list()) -> None:
+def hash_images_1(directory: str, hashes=list()) -> None:
     """
     NOTE
     The images are named like: C:\\Users\\Alex\\Desktop\\hello\\my_tinder_test\\tinder_pics\\311_Hannah_5.jpg
@@ -13,6 +14,7 @@ def hash_images(directory: str, hashes=list()) -> None:
     # Iterate over all the items in a directory, only hashing jpgs (can expand this to be other filetypes, if necessary)
     for item in tqdm(sorted(os.listdir(path=directory), key=lambda x: int(x.split('_')[0]))):
         if item.endswith('.jpg'):
+            # Append the hash
             hashes.append(str(imagehash.phash(image=Image.open(fp=os.path.join(directory, item)))))
 
     # Create a list of tuples which include only the image names and their respective hashes.
@@ -23,7 +25,7 @@ def hash_images(directory: str, hashes=list()) -> None:
                         hashes)
     )
 
-    # Write all of the hash values to a text file (can write to Excel or csv with pandas, if necessary)
+    # Write all of the image names and their respective hash values to a text file (can write to Excel or csv with pandas, if necessary)
     with open(file='./hash_images.txt', mode='w', encoding='utf-8-sig') as f:
         for i in range(len(hashes)):
             if i == (len(hashes) - 1):                               # If we're at the last image, don't add the new line
@@ -31,6 +33,34 @@ def hash_images(directory: str, hashes=list()) -> None:
             else:                                                    # Otherwise, we're not at the last image, so add the new line
                 f.write(objects[i][0] + '|' + objects[i][1] + '\n')
 
-# Call the function
+def hash_images_2(directory: str, records=list()) -> None:
+    """
+    NOTE
+    The images are named like: C:\\Users\\Alex\\Downloads\\Tinder is maddening\\maddy - 2.jpg
+
+    TODO
+    Potentially, we may want to calculate the hamming distance between images so that similar images, whose 
+    hashes don't match exactly, are seen as a match if they're defined as "similar enough" or something like that.
+    """
+
+    # Iterate over all the items in a directory, only hashing jpgs (can expand this to be other filetypes, if necessary)
+    for item in tqdm(sorted(os.listdir(path=directory), key=lambda x: int(x.split(' - ')[-1].split('.')[0]))):
+        if item.endswith('.jpg'):
+            # Append the image's name and the hash
+            records.append((item, str(imagehash.phash(image=Image.open(fp=os.path.join(directory, item))))))
+    
+    # Write the data to Excel
+    pandas_to_excel(data=records, records_path=r'C:\Users\Alex\Desktop\My_Hashes.xlsx', sheet_name='Hashes')
+
+def pandas_to_excel(data: list, records_path: str, sheet_name: str) -> None:
+        df = pd.DataFrame(data=data, columns=['File Name', 'Hash'])
+        df.to_excel(excel_writer=records_path, 
+                         sheet_name=sheet_name, 
+                         index=False, 
+                         freeze_panes=(1,0)
+                         )
+
+# Call the function(s)
 if __name__ == '__main__':
-    hash_images(directory=r'C:\Users\Alex\Desktop\hello\my_tinder_test\tinder_pics')
+    directory = input('Please input your directory:\n\t')
+    hash_images_2(directory=directory)
